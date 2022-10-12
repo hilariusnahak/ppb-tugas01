72195a8b9d5dd71fb1e739ef54f170becdf5100a
Hilarius Nahak <hilariusnahak6@gmail.com>
2022-10-12 22:41:19 +0700

    Firts Commit, the task does not have a function

---
+++ app/src/main/java/com/hilsriusnahak/tugas_01/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hilsriusnahak.tugas_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
